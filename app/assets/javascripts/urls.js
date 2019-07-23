543a8e0c7af903d1a2a00ec2f542022e551156ef
Diego Campoy <diego.campoy.collado@gmail.com>
2019-07-23 21:59:52 +0100

    Fix build assets

--- app/assets/javascripts/urls.js
+++ app/assets/javascripts/urls.js
@@ -1,7 +1,7 @@
 window.onload = function () {
-  const copyToClipboardButton = document.getElementById('url-details__copy')
+  var copyToClipboardButton = document.getElementById('url-details__copy')
   copyToClipboardButton && copyToClipboardButton.addEventListener('click', function (target) {
-    const shortenUrlInput = document.getElementById('url-details__input')
+    var shortenUrlInput = document.getElementById('url-details__input')
     shortenUrlInput.select()
     document.execCommand('copy')
     copyToClipboardButton.innerText = 'Copied!'
