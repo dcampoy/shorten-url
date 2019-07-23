fffee02e03e4a3026d51a959be68332c14e7d2c3
Diego Campoy <diego.campoy.collado@gmail.com>
2019-07-23 21:58:31 +0100

    Fix javascript error on homepage

--- app/assets/javascripts/urls.js
+++ app/assets/javascripts/urls.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   const copyToClipboardButton = document.getElementById('url-details__copy')
-  copyToClipboardButton.addEventListener('click', function (target) {
+  copyToClipboardButton && copyToClipboardButton.addEventListener('click', function (target) {
     const shortenUrlInput = document.getElementById('url-details__input')
     shortenUrlInput.select()
     document.execCommand('copy')
