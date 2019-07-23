583712b39ea02e846ee6205278ee21c70465939e
Diego Campoy <diego.campoy.collado@gmail.com>
2019-07-23 21:18:51 +0100

    Add copy to clipboard functionality

---
+++ app/assets/javascripts/urls.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  const copyToClipboardButton = document.getElementById('url-details__copy')
+  copyToClipboardButton.addEventListener('click', function (target) {
+    const shortenUrlInput = document.getElementById('url-details__input')
+    shortenUrlInput.select()
+    document.execCommand('copy')
+    copyToClipboardButton.innerText = 'Copied!'
+  })
+}
